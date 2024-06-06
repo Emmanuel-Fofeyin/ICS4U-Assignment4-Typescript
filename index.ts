29515f55d03eaa828c27485e03fdb90055ad2694
Emmanuel Israel Jr. <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-06-06 15:47:28 +0000

    lets try this

--- index.ts
+++ index.ts
@@ -34,7 +34,7 @@ const generatePattern = (number) => {
 };
 
 // Ask the user for a positive integer input
-createPrompt('Enter a positive integer: ')(answer => {
+createPrompt('Enter a positive integer: ') function getAnswer(answer => {
     const number = parseInt(answer);
 
     if (isNaN(number) || number < 1) {
